6838a97e05f6eff0424087649db2cc23da482c93
Sudhir Balaji <sud.balaji@gmail.com>
2023-12-11 23:42:31 +0000

    Day 4 Part 2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -31,6 +31,45 @@ fn part1() {
     println!("part 1: {}", points_sum);
 }
 
+fn part2() {
+    let input = include_str!("./input.txt");
+    let line_count = input.lines().count();
+
+    let mut card_counts = vec![1; line_count];
+
+    for (i, line) in input.lines().enumerate() {
+        let card_number_split: Vec<_> = line.split(":").collect();
+        let numbers_split: Vec<_> = card_number_split[1].split("|").collect();
+
+        let winning_numbers = numbers_split[0].split_whitespace();
+        let playing_numbers = numbers_split[1].split_whitespace();
+
+        let winning_numbers_set = winning_numbers.collect::<HashSet<_>>();
+        let playing_numbers_set = playing_numbers.collect::<HashSet<_>>();
+
+        let intersection = winning_numbers_set.intersection(&playing_numbers_set);
+
+        let winning_numbers: Vec<_> = intersection.collect();
+
+        let winning_numbers_count = winning_numbers.len();
+        if winning_numbers_count == 0 {
+            continue;
+        }
+
+        let start_index = i + 1;
+        let end_index = start_index + winning_numbers_count;
+
+        for _ in 0..card_counts[i] {
+            for j in start_index..end_index {
+                card_counts[j] += 1;
+            }
+        }
+    }
+
+    println!("part 2: {}", card_counts.iter().sum::<usize>());
+}
+
 fn main() {
     part1();
+    part2();
 }
