c75ecbb78be24fec9a88f6f48d6b3aa7aa3cc4bf
Sudhir Balaji <sud.balaji@gmail.com>
2023-12-27 23:56:22 +0000

    Day 9 Part 2

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -39,12 +39,44 @@ fn part1() {
         sum_of_extrapolated_numbers += carry;
     }
 
-    println!(
-        "Sum of extrapolated numbers: {}",
-        sum_of_extrapolated_numbers
-    );
+    println!("Part 1: {}", sum_of_extrapolated_numbers);
+}
+
+fn part2() {
+    let input = include_str!("./input.txt");
+
+    let mut sum_of_extrapolated_numbers = 0;
+
+    for line in input.lines() {
+        let numbers: Vec<isize> = line.split(" ").map(|x| x.parse().unwrap()).collect();
+
+        let mut first_elements = vec![numbers.first().unwrap().clone()];
+
+        let mut current_differences: Vec<isize> = get_differences(numbers);
+        let mut zero_differences = current_differences.iter().all(|&x| x == 0);
+
+        first_elements.push(current_differences.first().unwrap().clone());
+
+        while !zero_differences {
+            current_differences = get_differences(current_differences);
+            first_elements.push(current_differences.first().unwrap().clone());
+            zero_differences = current_differences.iter().all(|&x| x == 0);
+        }
+
+        first_elements.reverse();
+
+        let mut carry: isize = 0;
+        for i in 1..first_elements.len() {
+            carry = first_elements[i] - carry;
+        }
+
+        sum_of_extrapolated_numbers += carry;
+    }
+
+    println!("Part 2: {}", sum_of_extrapolated_numbers);
 }
 
 fn main() {
     part1();
+    part2();
 }
