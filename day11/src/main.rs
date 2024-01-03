50c1ea6f3f6d437392afdea965b3be1e15f68619
Sudhir Balaji <sud.balaji@gmail.com>
2024-01-03 23:17:14 +0000

    Day 11 part 2

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -81,6 +81,68 @@ fn part1() {
     println!("Part 1: {}", path_sum);
 }
 
+fn part2() {
+    let input = include_str!("./input.txt");
+    let grid = input
+        .lines()
+        .map(|line| line.chars().collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+
+    let mut hash_positions: Vec<(usize, usize)> = vec![];
+
+    for (i, row) in grid.iter().enumerate() {
+        for (j, c) in row.iter().enumerate() {
+            if *c == '#' {
+                hash_positions.push((i, j));
+            }
+        }
+    }
+
+    let mut x_increments: Vec<usize> = vec![0; hash_positions.len()];
+    let mut y_increments: Vec<usize> = vec![0; hash_positions.len()];
+
+    for (i, row) in grid.iter().enumerate() {
+        if row.iter().all(|&c| c == '.') {
+            for (j, hash_position) in hash_positions.iter().enumerate() {
+                if hash_position.0 > i {
+                    x_increments[j] += 999_999;
+                }
+            }
+        }
+    }
+
+    for (i, row) in transpose(&grid).iter().enumerate() {
+        if row.iter().all(|&c| c == '.') {
+            for (j, hash_position) in hash_positions.iter().enumerate() {
+                if hash_position.1 > i {
+                    y_increments[j] += 999_999;
+                }
+            }
+        }
+    }
+
+    for (i, hash_position) in hash_positions.iter_mut().enumerate() {
+        hash_position.0 += x_increments[i];
+        hash_position.1 += y_increments[i];
+    }
+
+    let mut path_sum: usize = 0;
+
+    for (first, second) in get_pairs(hash_positions) {
+        let (x2, y2) = second;
+        let (x1, y1) = first;
+
+        let x_diff: usize = (x2 as i32 - x1 as i32).abs().try_into().unwrap();
+        let y_diff: usize = (y2 as i32 - y1 as i32).abs().try_into().unwrap();
+
+        let distance = x_diff + y_diff;
+        path_sum += distance;
+    }
+
+    println!("Part 2: {}", path_sum);
+}
+
 fn main() {
     part1();
+    part2();
 }
